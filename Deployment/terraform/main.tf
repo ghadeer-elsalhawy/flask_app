61e05b405107b7b73599ee10568ca8fb35bda54a
Ghadeer Elsalhawy <ghadeer.elsalhawy@gmail.com>
2025-07-10 00:44:46 +0300

    Create Terraform Files

--- Deployment/terraform/main.tf
+++ Deployment/terraform/main.tf
@@ -1,50 +1,37 @@
-provider "aws" {
-    region = "eu-north-1"
+resource "tls_private_key" "ec2_private_key" {
+    algorithm = "RSA"
+    rsa_bits  = 4096
 }
 
-resource "aws_security_group" "adcash_sg" {
-    name        = "adcash-sg"
-
-    ingress {
-        description = "SSH"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        description = "HTTP"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+resource "aws_key_pair" "ec2_key" {
+    key_name   = "ec2-key"
+    public_key = tls_private_key.ec2_private_key.public_key_openssh
 }
 
-resource "aws_key_pair" "adcash_key" {
-    key_name   = "adcash-key"
-    public_key = file(".ssh/adcash_key.pub") 
+resource "local_file" "private_key_pem" {
+    content          = tls_private_key.ec2_private_key.private_key_pem
+    filename         = "${path.module}/../ec2-key.pem"
+    file_permission  = "0600"
 }
 
-resource "aws_instance" "adcash_vm" {
-    ami           = "ami-042b4708b1d05f512"
-    instance_type = "t3.micro"
-    security_groups = [aws_security_group.adcash_sg.name]
-    key_name        = aws_key_pair.adcash_key.key_name
-
-    tags = {
-        Name = "adcash-vm"
-    }
+resource "aws_instance" "flask_app" {
+    ami                    = "ami-0c94855ba95c71c99"  
+    instance_type          = "t2.micro"          
+    key_name               = aws_key_pair.ec2_key.key_name
+    vpc_security_group_ids = [aws_security_group.flask_app_sg.id]
 }
 
-resource "aws_eip" "adcash_eip" {
-    instance = aws_instance.adcash_vm.id
+resource "aws_eip" "app_eip" {
+    instance = aws_instance.flask_app.id
+}
+
+resource "local_file" "ansible_inventory" {
+    filename = "${path.module}/../ansible/inventory.ini"
+    content  = <<EOT
+    [webservers]
+    ${aws_eip.app_eip.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=../ec2-key.pem
+    EOT
+    file_permission = "0644"
+    
+    depends_on = [aws_eip.app_eip]
 }
